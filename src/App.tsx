56839658d3a68ff47e94efe12f8ec612f1094c5e
clement_genty <84876568+Aheuf@users.noreply.github.com>
2026-02-03 15:50:14 +0000

    wip: screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
-
 function App() {
+  const [isScreenshotActive, setIsScreenshotActive] = useState<boolean>(false)
+  const [imageSrc, setImageSrc] = useState()
+
+  const webcamRef = useRef(null)
+
+  const handleClick = () => {
+    setIsScreenshotActive(!isScreenshotActive)
+    if(isScreenshotActive) capture()
+  }
+
+  const capture = useCallback(
+    () => {
+      while(isScreenshotActive) {
+        setInterval(() => setImageSrc(webcamRef.current.getScreenshot()), 1000)
+      }
+    }, [webcamRef, isScreenshotActive])
+
   return (
     <div className="App">
       <h1>Poc Tesseract</h1>
-      <Webcam mirrored />
+      <div>
+        <Webcam mirrored/>
+        <button onClick={handleClick}>{isScreenshotActive ? "Stop" : "Start"} screenshot</button>
+      </div>
     </div>
   )
 }
-
+// https://blog.logrocket.com/using-react-webcam-capture-display-images/
+// https://www.npmjs.com/package/react-webcam
+// https://maheshrao98.hashnode.dev/building-a-real-time-video-text-recognition-app-using-tesseract-js-and-react
 export default App
